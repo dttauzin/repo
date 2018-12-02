3d9ebe28e40144788d4cce131efacb017e236ec6
SiDEUS Technologies <d.tauzin@gmail.com>
2018-12-02 08:37:33 -0600

    Update sirunner.sh

--- sirunner.sh
+++ sirunner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-	in-target wget http://sideustech.github.io/repo/sudoers -P /target/etc ;     \
-	in-target wget http://sideustech.github.io/repo/SiDEUS-SiLOX-OS-SPLASH.png -P /target/boot/grub ;     \
+	in-target /bin/cp sudoers /target/etc ;     \
+	in-target /bin/cp SiDEUS-SiLOX-OS-SPLASH.png /target/boot/grub ;     \
 	#in-target /bin/cp lsb-release /etc ;     \
 	#in-target /bin/cp grub /etc/default ;     \
 	#in-target /bin/cp sources.list /etc/apt ;     \
