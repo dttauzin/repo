07dcf3f6cff232dc0162529335c1d3c5248d50de
SiDEUS Technologies <d.tauzin@gmail.com>
2018-12-01 22:21:35 -0600

    Update sirunner.sh

--- sirunner.sh
+++ sirunner.sh
@@ -10,17 +10,17 @@
 #
 #
 #
-wget https://sideustech.github.io/repo/sudoers -P /target/etc
-wget https://sideustech.github.io/repo/SiDEUS-SiLOX-OS-SPLASH.png -P /target/boot/grub
-wget https://sideustech.github.io/repo/lsb-release -P /target/etc
-wget https://sideustech.github.io/repo/grub -P /target/etc/default
-wget https://sideustech.github.io/repo/sources.list -P /target/etc/apt
+cp https://sideustech.github.io/repo/sudoers /target/etc
+cp https://sideustech.github.io/repo/SiDEUS-SiLOX-OS-SPLASH.png /target/boot/grub
+cp https://sideustech.github.io/repo/lsb-release /target/etc
+cp https://sideustech.github.io/repo/grub /target/etc/default
+cp https://sideustech.github.io/repo/sources.list /target/etc/apt
 in-target mkdir /target/home/user/.config
 in-target mkdir /target/home/user/.config/autostart
-wget https://sideustech.github.io/repo/Start\ SiLOX\ OS\ Setup.desktop -P /target/home/user/.config/autostart
+cp https://sideustech.github.io/repo/Start\ SiLOX\ OS\ Setup.desktop /target/home/user/.config/autostart
 in-target chown -R user:user /target/home/user/.config
 in-target mkdir /target/home/user/System
 in-target mkdir /target/home/user/System/Scripts
-wget https://sideustech.github.io/repo/siboot.sh -P /target/home/user/System/Sctipts
+cp https://sideustech.github.io/repo/siboot.sh /target/home/user/System/Sctipts
 in-target chown -R user:user /target/home/user/System
 in-target /target/usr/sbin/update-grub
