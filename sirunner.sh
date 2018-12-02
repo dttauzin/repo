03e505443fe925cfa2f1def741787007d716c1a3
SiDEUS Technologies <d.tauzin@gmail.com>
2018-12-02 09:08:37 -0600

    Update sirunner.sh

--- sirunner.sh
+++ sirunner.sh
@@ -1,15 +1,16 @@
-
-	/target/bin/cp sudoers /target/etc;
-	#in-target /bin/cp http://sideustech.github.io/repo/SiDEUS-SiLOX-OS-SPLASH.png /boot/grub;
-	#in-target /bin/cp lsb-release /etc ;     \
-	#in-target /bin/cp grub /etc/default ;     \
-	#in-target /bin/cp sources.list /etc/apt ;     \
-	#in-target /bin/mkdir /home/user/.config ;     \
-	#in-target /bin/mkdir /home/user/.config/autostart ;     \
-	#in-target /bin/cp Start\ SiLOX\ OS\ Setup.desktop /home/user/.config/autostart ;     \
-	#in-target /bin/chown -R user:user /home/user/.config
-	#in-target /bin/mkdir /home/user/System ;     \
-	#in-target /bin/mkdir /home/user/System/Scripts ;     \
-	#in-target /bin/cp siboot.sh /home/user/System/Sctipts ;     \
-	#in-target /bin/chown -R user:user /home/user/System
-
+#!/bin/bash
+apt update && apt-get upgrade -y
+apt dist-upgrade -y
+cp sudoers /target/etc
+cp SiDEUS-SiLOX-OS-SPLASH.png /boot/grub
+cp lsb-release /etc
+cp grub /etc/default
+cp sources.list /etc/apt
+mkdir /home/user/.config
+mkdir /home/user/.config/autostart
+cp Start\ SiLOX\ OS\ Setup.desktop /home/user/.config/autostart
+chown -R user:user /home/user/.config
+mkdir /home/user/System
+mkdir /home/user/System/Scripts
+cp siboot.sh /home/user/System/Sctipts
+chown -R user:user /home/user/System
